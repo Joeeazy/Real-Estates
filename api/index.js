690e5c10b1fbf2c9dbd6992ad11064a9d40bfe0b
Joeeazy <joeeazy34@gmail.com>
2024-03-28 09:46:43 +0300

    backend

--- api/index.js
+++ api/index.js
@@ -6,5 +6,5 @@ const app = express();
 //methods to use
 //listen to port 3000
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!!");
 });
