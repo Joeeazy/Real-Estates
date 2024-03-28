77d4824a2af6565b67b11af855ed4d24750b7694
Joeeazy <joeeazy34@gmail.com>
2024-03-28 11:18:47 +0300

    user model updates

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -10,18 +10,19 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String, //type string
-      required: true, //mandatory to enter a userName
+      required: true, //mandatory to enter an emai
       unique: true, //should be unique
     },
     password: {
       type: String, //type string
-      required: true, //mandatory to enter a userName
+      required: true, //mandatory to enter a password
     },
   },
   { timestamps: true } //mongoDb should create a CREATED_AT timestamp and UPDATE_AT timestamp
 );
 
-// create a model
+// create a model for userSchema
 const User = mongoose.model("User", userSchema);
 
+//export the model created above
 export default User;
