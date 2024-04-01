3a79964f66747f46903a8d33c38a18123e7bda14
Joeeazy <joeeazy34@gmail.com>
2024-04-01 09:45:59 +0300

    implemented file storage in firebase

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,78 @@
 import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+
+  const [photo, setPhoto] = useState(undefined);
+
+  const [photopercentage, setPhotoPercentage] = useState(0);
+
+  const [photoUploadError, setPhotoUploadError] = useState(false);
+
+  const [formData, setFormData] = useState({});
+
+  console.log(photoUploadError);
+
+  console.log(photopercentage);
+
+  console.log(formData);
+
+  useEffect(() => {
+    if (photo) {
+      handlePhotoUpload(photo);
+    }
+  }, [photo]);
+
+  //
+  const handlePhotoUpload = (photo) => {
+    const photoStorage = getStorage(app);
+    const photoName = new Date().getTime() + photo.name;
+    const storageRef = ref(photoStorage, photoName);
+    const uploadTask = uploadBytesResumable(storageRef, photo);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //console.log("upload is" + progress + "% done");
+        setPhotoPercentage(Math.round(progress));
+      },
+
+      (error) => {
+        setPhotoUploadError(true);
+      },
+
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePic: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">My Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setPhoto(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.profilePic}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
