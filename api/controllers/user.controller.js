73a4f8f8dab67000f25454a6ad80b4b98ef4ae82
Joeeazy <joeeazy34@gmail.com>
2024-04-02 15:09:17 +0300

    delete user functionality done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -44,6 +44,8 @@ export const deleteUser = async (req, res, next) => {
   } //req.user.id = verifyUser.js  req.params.id = user.route.js
   try {
     await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.clearCookie("jwt");
     res.status(200).json("User has been succesfully deleted!");
   } catch (error) {
     next(error);
