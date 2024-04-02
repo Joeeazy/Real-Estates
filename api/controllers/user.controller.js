405eb4d47b3ba8de261a37ab6b1e563a1d833357
Joeeazy <joeeazy34@gmail.com>
2024-04-02 14:33:22 +0300

    updated user profile functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,7 +11,7 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "You can only update your own account"));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -37,3 +37,32 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+// export const updateUser = async (req, res, next) => {
+//   if (req.user.id !== req.params.id)
+//     return next(errorHandler(401, "You can only update your own account"));
+//   try {
+//     if (req.body.password) {
+//       req.body.password = bcryptjs.hashSync(req.body.password, 10);
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.params.id,
+//       {
+//         $set: {
+//           username: req.body.username,
+//           email: req.body.email,
+//           password: req.body.password,
+//           profilePic: req.body.profilePic,
+//         },
+//       },
+//       { new: true }
+//     );
+
+//     const { password, ...rest } = updatedUser._doc;
+
+//     res.status(200).json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import {
   getDownloadURL,
@@ -6,10 +6,17 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import {
+  updateUserBegin,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+
 import { app } from "../firebase";
+//import { async } from "@firebase/util";
 
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
 
   const [photo, setPhoto] = useState(undefined);
@@ -19,6 +26,10 @@ export default function Profile() {
   const [photoUploadError, setPhotoUploadError] = useState(false);
 
   const [formData, setFormData] = useState({});
+
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const dispatch = useDispatch();
 
   //console.log(photoUploadError);
 
@@ -60,10 +71,40 @@ export default function Profile() {
     );
   };
 
+  // function to handle input change
+  const handleUpdate = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  //function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserBegin());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">My Profile</h1>
-      <form className="flex flex-col gap-4">
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setPhoto(e.target.files[0])}
           type="file"
@@ -73,7 +114,7 @@ export default function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          src={formData.profilePic || currentUser.profilePic}
+          src={formData.profilePic || currentUser.profilePic} //?????????????????
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
@@ -93,32 +134,49 @@ export default function Profile() {
             ""
           )}
         </p>
+
         <input
           type="text"
           placeholder="username..."
-          id="username"
+          defaultValue={currentUser.username}
           className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleUpdate}
         />
+
         <input
-          type="text"
+          type="email"
           placeholder="email..."
+          defaultValue={currentUser.email}
           id="email"
           className="border p-3 rounded-lg"
+          onChange={handleUpdate}
         />
+
         <input
-          type="text"
+          type="password"
           placeholder="password..."
           id="password"
           className="border p-3 rounded-lg"
+          onChange={handleUpdate}
         />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          UPDATE
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading..." : "UPDATE"}
         </button>
       </form>
+
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-5700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User has been successfully updated" : ""}
+      </p>
     </div>
   );
 }
